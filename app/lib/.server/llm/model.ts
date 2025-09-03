8c5c42d98e847907d7ce3dfdf75e73bcc6434a34
yanfeng <yanfeng.qyf@alibaba-inc.com>
2025-09-03 21:23:11 +0800

    支持qwen

--- app/lib/.server/llm/model.ts
+++ app/lib/.server/llm/model.ts
@@ -1,4 +1,14 @@
 import { createAnthropic } from '@ai-sdk/anthropic';
+import { createQwen } from "qwen-ai-provider"
+
+export function getQwenModel() {
+  const qwen = createQwen({
+    apiKey: "sk-a1c78acf7a9e43c4b608402af1d9b30f",
+    baseURL:'https://dashscope.aliyuncs.com/compatible-mode/v1',
+    headers: { "Custom-Header": "value" },
+  })
+  return qwen('qwen3-coder-plus-2025-07-22');
+}
 
 export function getAnthropicModel(apiKey: string) {
   const anthropic = createAnthropic({
--- app/lib/.server/llm/stream-text.ts
+++ app/lib/.server/llm/stream-text.ts
@@ -1,6 +1,6 @@
 import { streamText as _streamText, convertToCoreMessages } from 'ai';
-import { getAPIKey } from '~/lib/.server/llm/api-key';
-import { getAnthropicModel } from '~/lib/.server/llm/model';
+import { getQwenModel } from '~/lib/.server/llm/model';
+
 import { MAX_TOKENS } from './constants';
 import { getSystemPrompt } from './prompts';
 
@@ -23,12 +23,9 @@ export type StreamingOptions = Omit<Parameters<typeof _streamText>[0], 'model'>;
 
 export function streamText(messages: Messages, env: Env, options?: StreamingOptions) {
   return _streamText({
-    model: getAnthropicModel(getAPIKey(env)),
+    model: getQwenModel() as any,
     system: getSystemPrompt(),
     maxTokens: MAX_TOKENS,
-    headers: {
-      'anthropic-beta': 'max-tokens-3-5-sonnet-2024-07-15',
-    },
     messages: convertToCoreMessages(messages),
     ...options,
   });
